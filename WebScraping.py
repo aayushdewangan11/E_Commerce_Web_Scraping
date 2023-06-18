50b8bbd93252856da16f8e99b056e82deab807de
Aayush Dewangan <aayushdewangan20@gmail.com>
2023-06-19 02:40:37 +0530

    first commit

---
+++ WebScraping.py
@@ -0,0 +1,44 @@
+import bs4
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
+
+url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+
+# opening url and grabbing the web page
+uClient = urlopen(url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, 'html.parser')
+
+# grabbing all containers with class name = item-container
+containers = page_soup.findAll('div', {'class': 'item-container'})
+
+filename = "products.csv"
+f = open(filename, 'w')
+
+headers = "brands, product_name, shipping\n"
+
+f.write(headers)
+
+##container = containers[1]
+
+if containers:  # Check if the containers list is not empty
+    for container in containers:
+        brand = container.div.div.a.img['title']
+        title_container = container.findAll('a', {'class': 'item-title'})
+        product_name = title_container[0].text
+        ship_container = container.findAll('li', {'class': 'price-ship'})
+        # use strip() to remove blank spaces before and after text
+        shipping = ship_container[0].text.strip()
+
+        print("brand:" + brand)
+        print("product_name:" + product_name)
+        print("shipping:" + shipping)
+
+        f.write(brand + ',' + product_name.replace(',', '|') + ',' + shipping + '\n')
+else:
+    print("No containers found.")
+
+f.close()
